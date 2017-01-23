8ca53dc94f74f623cf385977e31090ec3daaa1d4
moling3650 <365024424@qq.com>
2017-01-23 17:42:57 +0800

    封装客服端的获取数据接口的工具库

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,15 @@
 </template>
 
 <script>
-  export default {}
+  import { getChapterContent, getApiData } from 'common/js/utils.js'
+
+  export default {
+    created () {
+      getChapterContent('/api/link?chapter_id=1', (data) => console.log(data))
+      getApiData('/api/info', data => console.log(data))
+      getApiData('/api/detail', data => console.log(data))
+    }
+  }
 </script>
 
 <style>
---
+++ src/common/js/utils.js
@@ -0,0 +1,53 @@
+'use strict'
+const Vue = require('vue')
+const Base64 = require('js-base64').Base64
+const OK = 0
+
+function getContent (jsonpUrl, timeout = 3000) {
+  return new Promise((resolve, reject) => {
+    if (Object.prototype.toString.call(jsonpUrl) !== '[object String]') {
+      throw new Error('Type of param "jsonpUrl" is not string.')
+    }
+
+    const HEAD_NODE = document.querySelector('head')
+    const CALLBACK = 'duokan_fiction_chapter'
+    let timer
+    let paddingScript = document.createElement('script')
+    paddingScript.src = jsonpUrl
+
+    if (typeof timeout === 'number') {
+      timer = setTimeout(() => {
+        clearTimeout(timer)
+        HEAD_NODE.removeChild(paddingScript)
+        delete window[CALLBACK]
+        reject({ statusText: 'Request Timeout', status: 408 })
+      }, timeout)
+    }
+
+    window[CALLBACK] = (data) => {
+      clearTimeout(timer)
+      HEAD_NODE.removeChild(paddingScript)
+      delete window[CALLBACK]
+      resolve(JSON.parse(Base64.decode(data)))
+    }
+
+    HEAD_NODE.appendChild(paddingScript)
+  })
+}
+
+exports.getChapterContent = (url, callback, timeout) => {
+  Vue.http.get(url).then(res => {
+    if (res.body.result === OK) {
+      getContent(res.body.url, timeout).then(data => callback(data))
+    }
+  })
+}
+
+exports.getApiData = (url, callback) => {
+  Vue.http.get(url).then(res => {
+    if (res.body.result === OK) {
+      callback(res.body)
+    }
+  })
+}
+
